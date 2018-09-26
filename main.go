eb9dc73f5ef76e8b7365b29b458742467fb37a74
Julian Eng <julian3ng@gmail.com>
2018-09-26 16:24:27 -0700

    Create DB handle structure

--- main.go
+++ main.go
@@ -15,127 +15,87 @@ const (
 	dbname		= "todo"
 )
 
-func connect() (db, err) {
+
+type todo_db struct {
+	handle *sql.DB
+}
+
+func (t *todo_db) check_error(err error) {
+	if err != nil {
+		t.handle.Close()
+		panic(err)
+	}
+}
+
+
+// Connects and tests the connection
+// If the connection or the connection test fail, panic
+func (t *todo_db) init() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s " +
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	db, err := sql.Open("postgres", psqlInfo)
-	defer db.Close()
+	var err error
+	t.handle, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
-		return db, err
+		panic(err)
 	}
 	
-	if err = db.Ping(); err != nil {
-		return db, err
+	if err = t.handle.Ping(); err != nil {
+		t.handle.Close()
+		panic(err)
 	}
+	fmt.Println("Successfully initialized")
+}
 
-	fmt.Println("Successfully connected!")
+func (t *todo_db) reset() {
+	sqlStatement := `ALTER SEQUENCE todo_list_id_seq RESTART WITH 1`
+	_, err := t.handle.Exec(sqlStatement)
+	t.check_error(err)
+	fmt.Println("Successfully reset")
+}
 
-	return db, nil
+func (t *todo_db) destroy() {
+	t.handle.Close()
+	fmt.Println("Successfully destroyed")
+}
+
+func (t *todo_db) insert(item string) {
+	sqlStatement := `INSERT INTO todo_list (item) VALUES ($1)`
+	_, err := t.handle.Exec(sqlStatement, item)
+	t.check_error(err)
+	fmt.Println("Successfully inserted")
+}
+
+func (t *todo_db) work(id int) {
+	sqlStatement := `SELECT item FROM todo_list WHERE id = $1`
+	var item string
+	row := t.handle.QueryRow(sqlStatement, id)
+	switch err := row.Scan(&item); err {
+	case nil:
+		fmt.Println("Got: ", item)
+	case sql.ErrNoRows:
+		fmt.Println("Got nothing.")
+	default:
+		panic(err)
+	}
+}
+
+func (t *todo_db) delete(id int) {
+	sqlStatement := `DELETE FROM todo_list WHERE id = $1`
+	_, err := t.handle.Exec(sqlStatement, id)
+	t.check_error(err)
+	fmt.Println("Successfully deleted")
 }
 
 func main() {
 
-	db, err := connect()
+	var tdb todo_db
 
-	if err != nil {
-		panic(err)
-	}
-	
-
-	sqlStatement := `
-INSERT INTO users (age, email, first_name, last_name)
-VALUES ($1, $2, $3, $4)
-RETURNING id`
-	id := 0
-	err = db.QueryRow(sqlStatement, 30, "jon@gcalhoun.io", "Jonathan", "Calhoun").Scan(&id)
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.QueryRow(sqlStatement, 52, "bob@smith.io", "Bob", "Smith").Scan(&id)
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.QueryRow(sqlStatement, 15, "jerryjr123@gmail.com", "Jerry", "Seinfeld").Scan(&id)
-	if err != nil {
-		panic(err)
-	}	
-	
-	fmt.Println("New record ID is:", id)
-
-	sqlStatement = `
-UPDATE users
-SET first_name = $2, last_name = $3
-WHERE id = $1;`
-	_, err = db.Exec(sqlStatement, 1, "NewFirst", "NewLast")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully altered!")
-
-	sqlStatement = `SELECT id, email FROM users WHERE id=$1`
-	var email string
-
-	row := db.QueryRow(sqlStatement, 3)
-	
-	switch err := row.Scan(&id, &email); err {
-	case nil:
-		fmt.Println(id, email)		
-	case sql.ErrNoRows:
-		fmt.Println("No rows returned!")
-	default:
-		panic(err)
-	}
-
-
-	fmt.Println("=====Multiple row query=====")
-	rows, err := db.Query("SELECT id, first_name FROM users LIMIT $1", 3)
-	if err != nil {
-		panic(err)
-	}
-
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var firstName string
-		err = rows.Scan(&id, &firstName)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(id, firstName)
-	}
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("==========")
-
-	sqlStatement = `
-DELETE FROM users
-WHERE id > $1`
-
-	res, err := db.Exec(sqlStatement, 0)
-	if err != nil {
-		panic(err)
-	}
-
-	count, err := res.RowsAffected()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(count)
-
-	sqlStatement = `
-ALTER SEQUENCE users_id_seq RESTART WITH 1`
-	_, err = db.Exec(sqlStatement)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Reset the id")
+	tdb.init()
+	tdb.reset()
+	tdb.insert("Eat")
+	tdb.work(1)
+	tdb.delete(1)
+	tdb.destroy()
 }
